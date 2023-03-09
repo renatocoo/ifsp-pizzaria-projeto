bccca7855fb1cf60473b20d8c971eae69249f85f
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2023-03-09 20:24:22 -0300

    Titulo de outro cor

--- resources/views/components/feature-heading.blade.php
+++ resources/views/components/feature-heading.blade.php
@@ -1,3 +1,3 @@
-<h1 class="font-weight-bold text-center h1 my-5">{{ $title }}</h1>
+<h1 class="font-weight-bold text-center {{ $textcolor }} h1 my-5">{{ $title }}</h1>
 
 <p class="text-center grey-text mb-5 mx-auto w-responsive lead">{{ $body }}</p>
