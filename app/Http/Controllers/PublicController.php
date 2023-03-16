21cd15500e77ee29b6dd5e5227b490a271f1c451
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2023-03-16 20:34:46 -0300

    foreach associativo com request

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -21,7 +21,15 @@ class PublicController extends Controller
      
     public function save(Request $request) {
         //Sem injeção de request-> $obj= new Request();
+        //dd($request->all());
+        //dd($request);
+        //print_r($request->all());
+        //print_r($request->input('nome'));
 
+        $data = $request->input();
+        foreach($data as $key => $value) {
+            echo "$key => $value <br>";
+        }
 
         echo '
         <h1>Baby steps</h1><br><br><br><img src="https://suap.ifsp.edu.br/media/alunos/242781.y49d9HeEVfyy.jpg" height="100px" width="100px">
