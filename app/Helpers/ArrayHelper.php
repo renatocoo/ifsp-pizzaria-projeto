3de31f1aa61749f50e1851b054a8c873a18f7bfc
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2023-03-30 20:36:05 -0300

    html na func

--- app/Helpers/ArrayHelper.php
+++ app/Helpers/ArrayHelper.php
@@ -25,9 +25,39 @@ class ArrayHelper {
 
     public function getClientPhotos()
     {
-        return ['cards' => 'baby steps'];
+        $html ='';
+        $listaCards = '';
+
+        for ($i=0; $i < 12; $i++) { 
+           $html = $this->getCard();
+        }
+
+        return ['cards' => $html];
     }
 
+    private function getCard()
+    {
+        $html = '
+    <div class="card">
+
+
+        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.webp" alt="Card image cap">
+
+
+        <div class="card-body">
+
+
+            <h4 class="card-title"><a>Card title</a></h4>
+
+            <p class="card-text"> Some quick example text to build on the card title and make up the bulk of the cards
+                content.</p>
+
+            <a href="#" class="btn btn-primary">Button</a>
+
+        </div>
+
+    </div>'
+;
+        return $html;
+    }
 }
-
-
