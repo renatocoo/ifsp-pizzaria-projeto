bee30c9c1ade8b803d76f8e3e11adaf8a19d14c9
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2023-05-04 21:24:33 -0300

    autor e data em ingles ou pt

--- app/Helpers/PostHelper.php
+++ app/Helpers/PostHelper.php
@@ -5,11 +5,20 @@ use App\Helpers;
 class PostHelper extends DateTimeHelper {
 
     // Retorna o nome do autor de um post junto com a data de criação
-    public static function postAuthor($author, $date){
+    public static function postAuthor($author, $date, $english = false){
         $data = parent::UStoBRDate($date);
-        return "Criado por $author em: $data";
+        return $english ? 
+        self::message($author, $date) : 
+        self::mensagem($author, $data);
 
     }
 
+    private static function message($autor, $data){
+        return "Done by $autor in: $data";
+    }
+
+    private static function mensagem($autor, $data){
+        return "Criado por $autor em: $data";
+    }
 }
 ?>
